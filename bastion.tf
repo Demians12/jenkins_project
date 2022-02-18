fddde51a446d4052985c0ef9321d2115541426df
max <maxsantos_d@hotmail.com>
2022-02-18 02:35:55 -0300

    fixing bugs

--- bastion.tf
+++ bastion.tf
@@ -31,7 +31,7 @@ resource "aws_security_group" "bastion_host" {
 
   ingress {
       from_port = 0
-      to_port = 0
+      to_port = 22
       protocol = "tcp"
       cidr_blocks = ["0.0.0.0/0"]
   }
