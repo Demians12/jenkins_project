33835a60b6f3bd17f12672fe5ff3457d3ac11519
max <maxsantos_d@hotmail.com>
2022-02-17 21:03:15 -0300

    creating vpc, subnet and associations

---
+++ terraform.tfvars
@@ -0,0 +1,8 @@
+region="us-east-1"
+aws_profile = "default"
+author = "Max"
+cidr_block = "10.0.0.0/16"
+public_subnets_count = 2
+private_subnets_count = 2
+vpc_name = "management"
+availability_zones = ["us-east-1a", "us-east-1b"]
---
+++ variables.tf
@@ -0,0 +1,36 @@
+variable  "region" {
+    description = "AWS region"
+    type = string
+}
+
+variable "cidr_block" {
+  description = "VPC CIDR block"
+}
+
+variable "aws_profile" {
+  description = "bloco para VPC"
+  default = "default"
+  type = string
+}
+
+variable "vpc_name" {
+  description = "nome da vpc"
+}
+
+variable "Author" {
+  description = "name of the owner"
+  default = "None"
+  type = string
+}
+
+variable "availability_zones" {
+  type = list
+}
+
+variable "public_subnets_count" {
+  type = number
+}
+
+variable "private_subnets_count" {
+  type = number
+}
